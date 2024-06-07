cecac32d126a48706ed7409e662534c37a9f89c5
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-06-07 12:32:28 +0000

    Refactor:bottom Tabs icons and tab labels

--- screens/App/Tabs/layout.js
+++ screens/App/Tabs/layout.js
@@ -1,20 +1,112 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Welcome from '../Welcome';
-import WashDetails from '../WashDetails';
-import Tickets from '../Tickets';
-import Payments from '../Payments';
+import React, { useRef, useEffect } from 'react';
+import { TouchableOpacity, View, StyleSheet, Animated ,Text} from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../Welcome';
+import Reward from '../Payments';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const Stack = createNativeStackNavigator();
+import MakePost from '../Tickets';
+import Profile from '../WashDetails';
+import { useNavigation } from '@react-navigation/native';
 
-const AuthStack = () => {
+const Tab = createBottomTabNavigator();
+
+const AppTabs = () => {
+  const navigation = useNavigation();
+  const navToNotification = () => {
+    navigation.navigate('Notification');
+  };
   return (
-    <Stack.Navigator>
-      <Stack.Screen options={{headerShown:false}} name='Intro' component={Welcome}/>
-      <Stack.Screen options={{headerShown:false}} name='WashDetails' component={WashDetails}/>
-      <Stack.Screen options={{headerShown:false}} name='Tickets' component={Tickets}/>
-      <Stack.Screen options={{headerShown:false}} name='Payments' component={Payments}/>
-    </Stack.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let label;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+            label = 'Home';
+          } else if (route.name === 'History') {
+            iconName = focused ? 'stats-chart' : 'stats-chart-outline';
+            label = 'History';
+          } else if (route.name === 'Tickets') {
+            iconName = focused ? 'ticket' : 'ticket-outline';
+            label = 'Tickets';
+          } else if (route.name === 'Transactions') {
+            iconName = focused ? 'card' : 'card-outline';
+            label = 'Transactions';
+          }
+
+          return (
+            <View style={{ alignItems: 'center' }}>
+              <Ionicons name={iconName} size={20} color={color} />
+              <Text style={{ color, fontSize: 10 }}>{label}</Text>
+            </View>
+          );
+        },
+        tabBarActiveTintColor: 'dodgerblue',
+        tabBarInactiveTintColor: 'gray',
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          position: 'absolute',
+          height: 40,
+          borderTopWidth: 0,
+          elevation: 0,
+          backgroundColor: '#ffffff',
+          paddingBottom: 10,
+        },
+      })}
+    >
+      <Tab.Screen name="Home" component={Home}
+        options={{
+          headerTransparent: false,
+          headerTitle: () => (
+            <View style={{ height: "80%", display: "flex", flexDirection: "column", alignItems: 'center', justifyContent: "center" }}>
+              <Text style={{ fontSize: 18 }}>
+                Carwash
+              </Text>
+            </View>
+          ),
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#2328a0',
+            height: 80,
+          },
+          headerTitleStyle: {
+            fontSize: 15,
+            color: '#333',
+          },
+          headerRightContainerStyle: {
+            marginLeft: "2%"
+          }
+        }}
+      />
+      <Tab.Screen name="History" component={MakePost} />
+      <Tab.Screen name="Tickets" component={Reward} />
+      <Tab.Screen name="Transactions" component={Profile} />
+    </Tab.Navigator>
   );
 };
 
-export default AuthStack;
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowColor: '#7F5DF0',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+  middleButton: {
+    width: 55,
+    height: 55,
+    borderRadius: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default AppTabs;
--- screens/App/Welcome.js
+++ screens/App/Welcome.js
@@ -11,67 +11,7 @@ import { FontAwesome ,Feather} from "@expo/vector-icons";
 export default function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.txtHead}>Welcome User,</Text>
-        <Text style={styles.txtSub}>
-          Lorem ipsum thei ,rfurfin jhyu iuruibrkf uhd
-        </Text>
-      </View>
-      <Text style={styles.txtHead2}>Reports</Text>
-      <Text style={styles.txtSub2}>
-        Lorem ipsum thei ,rfurfin jhyu iuruibrkf uhd
-      </Text>
-      <TouchableOpacity style={styles.BtnAllReports}>
-        <FontAwesome
-          name="file-o"
-          size={17}
-          color="white"
-          style={{ alignSelf: "left", marginLeft: "5%", marginTop: "5%" }}
-        />
-        <Text style={styles.BtnTxt}>All Reports</Text>
-        <Feather name="arrow-right-circle" size={17} color="white" style={{marginLeft:"55%"}} />
-      </TouchableOpacity>
-      <Text style={styles.txtHead2}>Activity</Text>
-      <Text style={styles.txtSub2}>
-        Select an option to continue
-      </Text>
-      <TouchableOpacity style={styles.BtnAllReports}>
-        <FontAwesome
-          name="file-o"
-          size={17}
-          color="white"
-          style={{ alignSelf: "left", marginLeft: "5%", marginTop: "5%" }}
-        />
-        <Text style={styles.BtnTxt}>Get Car Wash Details</Text>
-        <Feather name="arrow-right-circle" size={17} color="white" style={{marginLeft:"34%"}} />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.BtnAllReports}>
-        <FontAwesome
-          name="file-o"
-          size={17}
-          color="white"
-          style={{ alignSelf: "left", marginLeft: "5%", marginTop: "5%" }}
-        />
-        <Text style={styles.BtnTxt}>Find a Ticket or Service</Text>
-        <Feather name="arrow-right-circle" size={17} color="white" style={{marginLeft:"30%"}} />
-      </TouchableOpacity>
-      <Text style={styles.txtHead2}>Others</Text>
-      <Text style={styles.txtSub2}>
-        Select an option to continue
-      </Text>
-      <TouchableOpacity style={styles.BtnAllReports}>
-        <FontAwesome
-          name="file-o"
-          size={17}
-          color="white"
-          style={{ alignSelf: "left", marginLeft: "5%", marginTop: "5%" }}
-        />
-        <Text style={styles.BtnTxt}>Find a Ticket or Service</Text>
-        <Feather name="arrow-right-circle" size={17} color="white" style={{marginLeft:"30%"}} />
-      </TouchableOpacity>
-      <View style={styles.Footer}>
-
-      </View>
+      
     </SafeAreaView>
   );
 }
@@ -80,60 +20,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-alignItems:"center"
-  },
-  headerContainer: {
-    backgroundColor: "#007B5D",
-    width: "100%",
-    height: 100,
-    justifyContent: "center",
-    alignSelf: "top",
-  },
-  txtHead: {
-    color: "#fff",
-    fontSize: 25,
-    fontWeight: "bold",
-    marginLeft: 10,
-  },
-  txtSub: {
-    color: "#fff",
-    fontSize: 13,
-    marginLeft: 13,
-  },
-  txtHead2: {
-    color: "#000",
-    fontSize: 17,
-    fontWeight: "bold",
-    textAlign: "left",
-    marginLeft: 12,
-    marginTop: 5,
-  },
-  txtSub2: {
-    color: "#000",
-    fontSize: 12,
-    marginLeft: 13,
-  },
-  BtnAllReports: {
-    backgroundColor: "#161829",
-    width: "95%",
-    height: 50,
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-    borderRadius: 5,
-    marginTop: 10,
-    marginLeft: 10,
-  },
-  BtnTxt: {
-    color: "#fff",
-    fontSize: 15,
-    textAlign:"left",
-    marginLeft:"2%"
-  }, 
-    Footer: {
-        backgroundColor: "#007B5D",
-        width: "100%",
-        height: 50,
-        alignSelf: "bottom",
-    },
+  }
 });
