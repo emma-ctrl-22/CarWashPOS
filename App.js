402a0a9559eee9b17f507dc2fc8e44b3630965db
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-06-07 11:44:44 +0000

    start

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Welcome from './screens/Welcome';
+import WashDetails from './screens/WashDetails';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="BottomTabs">
+      <Stack.Screen name="BottomTabs" component={Welcome} options={{ headerShown: false }} />
+      <Stack.Screen name="AddReminder" component={WashDetails} options={{ headerShown: false }}/>
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
