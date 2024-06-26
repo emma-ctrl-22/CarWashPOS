48b508ddf259694deba7b390da0af42c83dc072f
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-06-26 10:21:36 +0000

    refactor: Update TicketDetails screen to improve UI and add functionality
    This commit refactors the TicketDetails screen to improve the user interface and add new functionality. It adds a ticket container with a background color, padding, and shadow to enhance the visual appearance. It also includes two new buttons, "Close Ticket" and "Make Transaction", with corresponding styles. Additionally, it aligns the content in the center and adjusts the width of the elements for better layout. The changes aim to enhance the user experience and provide a more intuitive interface for managing tickets.

--- screens/App/TicketDetails.js
+++ screens/App/TicketDetails.js
@@ -1,29 +1,62 @@
-import React from 'react'
-import { View, Text,StyleSheet } from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const TicketDetails = ({route}) => {
+const TicketDetails = ({ route }) => {
   const ticket = route.params.ticket;
   return (
     <View style={styles.container}>
-    <Text style={styles.text}>Ticket Number: {ticket.ticket_number}</Text>
-    <Text style={styles.text}>Start Time: {ticket.start_time}</Text>
-    <Text style={styles.text}>Price: {ticket.price}</Text>
-    <Text style={styles.text}>Car Number: {ticket.car_number}</Text>
-    
-  </View>
-  )
-}
+      <View style={styles.ticketContainer}>
+        <Text style={styles.text}>Ticket Number: {ticket.ticket_number}</Text>
+        <Text style={styles.text}>Start Time: {ticket.start_time}</Text>
+        <Text style={styles.text}>Price: {ticket.price}</Text>
+        <Text style={styles.text}>Car Number: {ticket.car_number}</Text>
+      </View>
+        <TouchableOpacity style={styles.closeBtn}>
+          <Text style={styles.Btntext}>Close Ticket</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.closeBtn}>
+          <Text style={styles.Btntext}>Make Transaction</Text>
+        </TouchableOpacity>
+    </View>
+  );
+};
 
-export default TicketDetails
+export default TicketDetails;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
+    alignItems: "center",
   },
   text: {
     fontSize: 18,
     marginBottom: 10,
   },
+  ticketContainer:{
+    padding: 16,
+    backgroundColor: '#e3e5fe',
+    marginBottom: 8,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 1,
+    width:"98%"
+  },
+  closeBtn:{
+    backgroundColor: '#2328a0',
+    padding: 14,
+    borderRadius: 8,
+    width:"98%",
+    alignItems: "center",
+    marginTop: 10,
+    justifyContent: "center",
+  },
+  Btntext:{
+    color: "white",
+    fontSize: 18,
+  }
 });
